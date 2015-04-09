ab0269661d7a4fc9dcdcce9bb581a65726aa2262
Jason <github@jasonpoon.ca>
2015-04-09 16:30:09 -0700

    add plugins

---
+++ scripts/camera.js
@@ -0,0 +1,34 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', setupCamera, false );
+
+    function setupCamera() {
+        // camera
+        document.querySelector("#photoCanvas").addEventListener("touchend", startCamera, false);
+
+        var cameraPopoverHandle;
+
+        function startCamera() {
+            navigator.camera.getPicture(onSuccess, onFail,
+             {
+                 destinationType: Camera.DestinationType.FILE_URI,
+                 sourceType: Camera.PictureSourceType.CAMERA,
+                 correctOrientation: true,
+             });
+        }
+
+        function onSuccess(u) {
+            document.querySelector("#photoCanvas").src = u;
+        }
+
+        function onFail(e) {
+            console.log('onFail');
+            console.dir(e);
+        }
+    };
+} )();
--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,6 @@
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
 
     function onPause() {
